ebd848089f84e75409c5190d64b2f2e3d67e621d
Casey Duncan <casey.duncan@gmail.com>
2011-09-18 21:18:41 -0600

    Basic user profile impl

---
+++ gravita/test/test_user_profile.py
@@ -0,0 +1,31 @@
+import unittest
+
+class UserProfileTestCase(unittest.TestCase):
+
+    def tearDown(self):
+        from gravita import user_profile
+        user_profile.clear_all()
+
+    def test_unique_id(self):
+        from gravita.user_profile import UserProfile
+        ids = set()
+        for i in range(500):
+            up = UserProfile()
+            self.assertEqual(up.game, None)
+            self.assertTrue(up.profile_id)
+            self.assertTrue(isinstance(up.profile_id, str))
+            self.assertTrue(up.profile_id not in ids)
+            ids.add(up.profile_id)
+
+    def test_load_profile(self):
+        from gravita.user_profile import UserProfile
+        ups = [UserProfile() for i in range(10)]
+        for i, p in enumerate(up for up in ups):
+            self.assertTrue(UserProfile.load(p.profile_id) is ups[i])
+
+    def test_load_profile_wrong_id(self):
+        from gravita.user_profile import UserProfile
+        self.assertRaises(KeyError, UserProfile.load, 'flombom')
+        
+
+
---
+++ gravita/user_profile.py
@@ -0,0 +1,43 @@
+##############################################################################
+#
+# Copyright (c) 2011 Casey Duncan
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the MIT License
+# A copy of the license should accompany this distribution.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#
+#############################################################################
+import random
+import sys
+
+_profiles = {}
+
+def clear_all():
+    """Clear all persisted User profiles"""
+    global _profiles
+    _profiles.clear()
+
+
+class UserProfile(object):
+
+    def __init__(self):
+        global _profiles
+        while 1:
+            self.profile_id = self.generate_id()
+            if self.profile_id not in _profiles:
+                _profiles[self.profile_id] = self
+                break
+        self.game = None
+
+    @classmethod
+    def load(self, profile_id):
+        """Load a UserProfile instance from persistence"""
+        return _profiles[profile_id]
+
+    def generate_id(self):
+        """Generate a unique id for this profile"""
+        return hex(id(self) ^ random.randint(sys.maxint >> 16, sys.maxint))[2:]
+
