5ec1e0cac2b3127a56df97366969cba3c2b6654b
Casey Duncan <casey.duncan@gmail.com>
2011-10-03 23:47:43 -0600

    Fix __init__.py test

--- gravita/test/test_init.py
+++ gravita/test/test_init.py
@@ -10,6 +10,8 @@ class InitTestCase(unittest.TestCase):
         self.assertTrue(isinstance(config, TestConfigFactory))
         self.assertTrue(config.wsgi_app_made)
         self.assertEqual(config.settings, {'foo': 'bar'})
+        self.assertEqual(
+            config.static_view, {'name': '/static', 'path': 'www'})
         self.assertEqual(config.scanned, 'gravita')
 
 
@@ -18,7 +20,11 @@ class TestConfigFactory(object):
     def __init__(self, settings):
         self.settings = settings
         self.wsgi_app_made = False
+        self.static_view = None
         self.scanned = None
+
+    def add_static_view(self, **kw):
+        self.static_view = kw
 
     def scan(self, what):
         self.scanned = what
